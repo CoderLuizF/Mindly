41fb62019eceee1a8926da3a6706edd7741126e6
Luiz Felipe <zzzezizzz@gmail.com>
2025-12-02 08:59:55 -0300

    feat(dashboard): create base dashboard structure and routing

--- routes/toughtRoutes.js
+++ routes/toughtRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 //Controller
 const ToughtController = require("../controllers/ToughtController");
 
+router.get("/dashboard", ToughtController.dashboard);
 router.get("/", ToughtController.showToughts);
 
 module.exports = router;
