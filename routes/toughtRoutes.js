3726d896f525117053192ece352f6f7936bf3473
Luiz Felipe <zzzezizzz@gmail.com>
2025-12-08 11:45:31 -0300

    feat(toughts): implement thought deletion with authorization check

--- routes/toughtRoutes.js
+++ routes/toughtRoutes.js
@@ -7,9 +7,10 @@ const ToughtController = require("../controllers/ToughtController");
 // Helpers
 const checkAuth = require("../helpers/auth").checkAuth;
 
-router.get("/add", checkAuth, ToughtController.createTought)
-router.post("/add", checkAuth, ToughtController.createToughtSave)
+router.get("/add", checkAuth, ToughtController.createTought);
+router.post("/add", checkAuth, ToughtController.createToughtSave);
 router.get("/dashboard", checkAuth, ToughtController.dashboard);
+router.post("/remove", checkAuth, ToughtController.removeTought);
 router.get("/", ToughtController.showToughts);
 
 module.exports = router;
