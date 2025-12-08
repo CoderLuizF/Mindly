08d05419d2a0c9475a70a6f3fd85d3dfe2f9d098
Luiz Felipe <zzzezizzz@gmail.com>
2025-12-08 16:46:59 -0300

    feat(search): implement thought search functionality with results feedback

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -2,15 +2,32 @@ const { where } = require("sequelize");
 const Mindly = require("../models/Mindly");
 const User = require("../models/User");
 
+const { Op } = require("sequelize");
+
 module.exports = class ToughtController {
   static async showToughts(req, res) {
+    let search = "";
+
+    if (req.query.search) {
+      search = req.query.search;
+    }
+
     const toughtsData = await Mindly.findAll({
       include: User,
+      where: {
+        title: { [Op.like]: `%${search}%` },
+      },
     });
 
     const toughts = toughtsData.map((result) => result.get({ plain: true }));
 
-    res.render("toughts/home", { toughts });
+    let toughtsQty = toughts.length;
+
+    if (toughtsQty === 0) {
+      toughtsQty = false;
+    }
+
+    res.render("toughts/home", { toughts, search, toughtsQty });
   }
 
   static async dashboard(req, res) {
